66da61d36037a8d8be8c50a4d1a4201144c0fd05
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-30 21:45:00 -0400

    created models

---
+++ portfolio_app/models.py
@@ -0,0 +1,37 @@
+from portfolio_app import db
+from datetime import datetime
+
+
+class User(db.Model):
+  __tablename__ = "user"
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), nullable=False)
+  password = db.Column(db.String(80), nullable=False)
+  email = db.Column(db.String(80), nullable=False)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+  created_portfolios = db.relationship("Portfolio", back_populates="user")
+
+class Portfolio(db.Model):
+  __tablename__ = "portfolio"
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+  bio = db.Column(db.String, nullable=False)
+  skills = db.Column(db.String, nullable=False)
+  projects = db.Column(db.String, nullable=False)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+  user = db.relationship("User", back_populate="created_portfolios")
+  project_list = db.relationship("Project", back_populate="portfolio")
+
+class Project(db.Model):
+  __tablename__ = "project"
+  id = db.Column(db.Integer, primary_key=True)
+  portfolio_id = db.Column(db.Integer, db.ForeignKey("portfolio.id"))
+  title = db.Column(db.String(100), nullable=False)
+  description = db.Column(db.String, nullable=False)
+  github_link = db.Column(db.String, nullable=False)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+  portfolio = db.relationship("Portfolio", back_populate="project_list")
