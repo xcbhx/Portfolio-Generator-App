9720481807d28de30e5f349ddfa009ffe608c202
Ceina Ellison <ceina.ellison@gmail.com>
2025-05-05 21:12:55 -0400

    created user, portfolio, & projects forms

---
+++ portfolio_app/forms.py
@@ -0,0 +1,56 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, URL, Email
+
+class UserForm(FlaskForm):
+  """Form for adding User."""
+  username = StringField("Username",
+    validators=[
+      DataRequired(),
+      Length(min=3, max=100, message="Your username must be between 3 and 100 characters.")
+    ])
+  password = PasswordField("Password",
+    validators=[
+      DataRequired(),
+      Length(min=3, max=80, message="Your password must be between 3 and 80 characters.")
+    ])
+  email = StringField("Email",
+    validators=[
+      DataRequired(),
+      Email(message="Please enter a valid URL."),
+      Length(min=5, max=80, message="Your email must be between 5 and 80 characters.")
+    ])
+  submit = SubmitField("Create Account")
+
+
+class PortfolioForm(FlaskForm):
+  bio = StringField("Bio", 
+    validators=[
+      DataRequired(),
+      Length(min=3, max=100, message="Your bio must be between 3 and 100 characters.")
+    ])
+  skills = StringField("Skills", 
+    validators=[
+      DataRequired(),
+      Length(min=3, message="Please enter at least 3 characters for skills.")
+    ])
+  submit = SubmitField("Create Portfolio")
+
+
+class ProjectForm(FlaskForm):
+  title = StringField("Project Title", 
+    validators=[
+      DataRequired(),
+      Length(min=3, max=100, message="Your title must be between 3 and 100 characters.")
+    ])
+  description = StringField("Project Description", 
+    validators=[
+      DataRequired(),
+      Length(min=3, max=200, message="Description must be between 3 and 200 characters.")
+    ])
+  github_link = StringField("Github Link", 
+    validators=[
+      DataRequired(),
+      URL(message="Please enter a valid URL.")
+    ])
+  submit = SubmitField("Add Project")
